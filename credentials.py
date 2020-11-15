e5c0f20e2c3ee511ad6c2b2e018bb8e1b6d3c532
claudinemahoro <claudinemah3@gmail.com>
2020-11-15 23:41:55 +0200

    Create a method to define the account to be saved's object

--- credentials.py
+++ credentials.py
@@ -6,3 +6,12 @@ class Credential:
 	list_of_credentials =[]
 	user_credentials_list = []
 
+  def __init__(self,username,platform_name,account_name,password):
+		'''
+		Method to define the properties for each user's acounts object.
+		'''
+
+		self.username = username
+		self.platform_name = platform_name
+		self.account_name = account_name
+		self.password = password
