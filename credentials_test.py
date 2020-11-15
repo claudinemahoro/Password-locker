f4792cb2c0a2d341990921fe1c517e09fd08d9dc
claudinemahoro <claudinemah3@gmail.com>
2020-11-16 00:33:24 +0200

    checking if the new user's info was saved

--- credentials_test.py
+++ credentials_test.py
@@ -25,5 +25,12 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.last_name,"Mahoro")
         self.assertEqual(self.new_user.password,"2580")
        
+    def test_save_user(self):
+        '''
+		Test to check if the new users info is saved into the users list
+		'''
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list),1)
+        
 if __name__ == '__main__':
     unittest.main()
