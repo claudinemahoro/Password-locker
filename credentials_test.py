3c05294948dd0f765151f664f2eb875c4f63e5f1
claudinemahoro <claudinemah3@gmail.com>
2020-11-16 00:22:53 +0200

    importing the needed classes and modules and creating a TestUser class for testing the entered credentials

---
+++ credentials_test.py
@@ -0,0 +1,11 @@
+
+import unittest
+from user import User  
+from credentials import Credential 
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines the test cases for the the user class behaviours.
+
+    '''
