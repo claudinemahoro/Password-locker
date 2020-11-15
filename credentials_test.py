566bab0a8efac072797b07fb113226e6fb0a1c96
claudinemahoro <claudinemah3@gmail.com>
2020-11-16 00:31:10 +0200

    create setUp method and test_init

--- credentials_test.py
+++ credentials_test.py
@@ -9,3 +9,21 @@ class TestUser(unittest.TestCase):
     Test class that defines the test cases for the the user class behaviours.
 
     '''
+    
+     def setUp(self):
+        '''
+        Setup method to run before each test case 
+        ''' 
+        self.new_user = User('Claudine', 'Mahoro', '2580)
+    
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"Claudine")
+        self.assertEqual(self.new_user.last_name,"Mahoro")
+        self.assertEqual(self.new_user.password,"2580")
+       
+if __name__ == '__main__':
+    unittest.main()
