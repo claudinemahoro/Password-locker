154d1e99011b4c4bd9bed2d6e6e53a50cfea1895
claudinemahoro <claudinemah3@gmail.com>
2020-11-16 02:06:57 +0200

    add readme file

--- credentials.py
+++ credentials.py
@@ -10,17 +10,17 @@ class Credential:
 	list_of_credentials =[]
 	user_credentials_list = []
 
-  def __init__(self,username,platform_name,account_name,password):
-		'''
+  def __init__(self,username,platform_name,account_name,password) :
+
+	  '''
 		Method to define the properties for each user's acounts object.
 		'''
-
 		self.username = username
 		self.platform_name = platform_name
 		self.account_name = account_name
 		self.password = password
 
-  @classmethod
+   @classmethod
 	def check_user(cls,first_name,password):
 		'''
 		Method that checks if the first-name and password entered by the user already exist in the users_list
